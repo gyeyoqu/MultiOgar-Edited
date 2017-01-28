e5deee6800da7185f53ff7883f3196ae9866c790
Luka967 <luka.djokovic2@gmail.com>
2017-01-28 15:16:04 +0100

    Update PlayerCell.js

--- src/entity/PlayerCell.js
+++ src/entity/PlayerCell.js
@@ -11,28 +11,32 @@ PlayerCell.prototype = new Cell();
 
 // Main Functions
 
-PlayerCell.prototype.canEat = function (cell) {
+PlayerCell.prototype.canEat = function(cell) {
     return true; // player cell can eat anyone
 };
 
-PlayerCell.prototype.getSpeed = function (dist) {
+PlayerCell.prototype.getSpeed = function(dist) {
     var speed = 2.1106 / Math.pow(this._size, 0.449);
-    var normalizedDist = Math.min(dist, 32) / 32;
+    var normalizedDist = Math.min(dist, 32) * 0.03125;
     speed *= 40 * this.gameServer.config.playerSpeed;
     return speed * normalizedDist / dist;
 };
 
-PlayerCell.prototype.onAdd = function (gameServer) {
+PlayerCell.prototype.onAdd = function(gameServer) {
+    gameServer.nodesPlayer.push(this);
+
     // Gamemode actions
     gameServer.gameMode.onCellAdd(this);
 };
 
-PlayerCell.prototype.onRemove = function (gameServer) {
+PlayerCell.prototype.onRemove = function(gameServer) {
     // Remove from player cell list
     var index = this.owner.cells.indexOf(this);
-    if (index != -1) {
-        this.owner.cells.splice(index, 1);
-    }
+    if (index != -1) this.owner.cells.splice(index, 1);
+
+    index = gameServer.nodesPlayer.indexOf(this);
+    if (index != -1) gameServer.nodesPlayer.splice(index, 1);
+
     // Gamemode actions
     gameServer.gameMode.onCellRemove(this);
 };
